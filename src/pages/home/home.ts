9000ffc57982c3d7a2195867c369003f06c982a4
juanpabloletteri <pabloletteri@gmail.com>
2017-09-24 00:32:36 -0300

    nuevo login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,25 +13,33 @@ export class HomePage {
   usuario: string;
   email: string;
   pass: string;
+  perfil: string;
 
+  pass1: string;
+  pass2: string;
+
+  esUsuario: boolean = true;
   usuarios: FirebaseListObservable<any>;
 
   constructor(public navCtrl: NavController, public alertCtrl: AlertController, db: AngularFireDatabase, ) {
     this.usuarios = db.list('/usuarios');
-    console.log(this.usuarios);
   }
   login() {
     this.usuarios.forEach(element => {
-      for (var i = 0; i < 5; i++) {
+      for (let i in element) {
+        console.log(i);
         if (element[i].nombre == this.usuario && element[i].clave == this.pass) {
           //SE ENCONTRO USUARIO
+          this.usuario = element[i].nombre;
+          this.perfil = element[i].perfil;
+
           let alert = this.alertCtrl.create({
             title: 'Bienvenido: ' + this.usuario,
             buttons: ['OK']
           });
           alert.present();
           //Redirijo a la pagina correspondiente
-          this.navCtrl.push(AplicacionPage, { "usuario": this.usuario, "email": this.email });
+          this.navCtrl.push(AplicacionPage, { "usuario": this.usuario, "perfil": this.perfil });
           return;
         }
       }
@@ -65,5 +73,34 @@ export class HomePage {
       this.email = "";
       this.pass = "";
     }*/
+  }
+  asignarUsuario(tipo) {
+    switch (tipo) {
+      case "admin": {
+        this.usuario = "admin";
+        this.pass = "11";
+        break;
+      }
+      case "invitado": {
+        this.usuario = "invitado";
+        this.pass = "22";
+        break;
+      }
+      case "usuario": {
+        this.usuario = "usuario";
+        this.pass = "33";
+        break;
+      }
+      case "j1": {
+        this.usuario = "j1";
+        this.pass = "44";
+        break;
+      }
+      case "j2": {
+        this.usuario = "j2";
+        this.pass = "55";
+        break;
+      }
+    }
   }
 }
